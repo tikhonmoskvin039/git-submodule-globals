9f605aa0b18855db511a36e3f39a172b4574b482
Tikhon Moskvin <tihonoskvin@mail.ru>
2023-06-20 23:15:50 +0300

    sub upd 2

--- components/MyButton.tsx
+++ components/MyButton.tsx
@@ -1,9 +1,9 @@
 import { useNavigate } from "react-router-dom"
 
-const MyButton = () => {
+function MyButton() {
   const navigate = useNavigate()
   return (
-    <div onClick={() => navigate('/subsub')} style={{width: 100, height: 100, backgroundColor: 'red'}}>MyButton</div>
+    <div onClick={() => navigate('/subsub')} style={{width: '100px', height: '100px', backgroundColor: 'red', marginBottom: 20}}>MyButton</div>
   )
 }
 
